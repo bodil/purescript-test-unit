ee3450eb9aeaa15752c22a9cc0ff558214e557b6
Pascal Hartig <phartig@twitter.com>
2015-08-31 23:57:50 +0200

    Upgrade to transformers ~0.7

--- src/Test/Unit.purs
+++ src/Test/Unit.purs
@@ -23,12 +23,12 @@ import Prelude
 import Control.Monad.Cont.Trans
 import Control.Monad.Eff
 import Control.Monad.Eff.Ref
-import Control.Monad.Error.Trans
+import Control.Monad.Except.Trans
 import Data.Either
 import Test.Unit.Console
 
 type TestResult = Either String Unit
-type TestUnit e = ErrorT String (ContT Unit (Eff e)) Unit
+type TestUnit e = ExceptT String (ContT Unit (Eff e)) Unit
 type Assertion e = TestUnit e
 type Test e = TestUnit e
 
@@ -39,9 +39,9 @@ failure :: String -> TestResult
 failure = Left
 
 pickFirst :: forall e. TestUnit (ref :: REF | e) -> TestUnit (ref :: REF | e) -> TestUnit (ref :: REF | e)
-pickFirst t1 t2 = ErrorT $ ContT $ \cb -> do
+pickFirst t1 t2 = ExceptT $ ContT $ \cb -> do
   yielded <- newRef false
-  let yield t = runContT (runErrorT t) \res -> do
+  let yield t = runContT (runExceptT t) \res -> do
         hasYielded <- readRef yielded
         writeRef yielded true
         if hasYielded then return unit else cb res
@@ -55,26 +55,26 @@ foreign import setTimeout :: forall e a. Int -> Eff (timer :: Timer | e) a -> Ef
 
 timeout :: forall e. Int -> TestUnit (timer :: Timer, ref :: REF | e) -> TestUnit (timer :: Timer, ref :: REF | e)
 timeout time test = pickFirst test $
-  ErrorT $ ContT \cb -> do
+  ExceptT $ ContT \cb -> do
   setTimeout time $ do cb $ failure $ "test timed out after " ++ show time ++ "ms"
   return unit
 
 assert :: forall e. String -> Boolean -> Assertion e
-assert _ true = ErrorT $ ContT \cb -> cb success
-assert reason false = ErrorT $ ContT \cb -> cb $ failure reason
+assert _ true = ExceptT $ ContT \cb -> cb success
+assert reason false = ExceptT $ ContT \cb -> cb $ failure reason
 
 assertFalse :: forall e. String -> Boolean -> Assertion e
-assertFalse _ false = ErrorT $ ContT \cb -> cb success
-assertFalse reason true = ErrorT $ ContT \cb -> cb $ failure reason
+assertFalse _ false = ExceptT $ ContT \cb -> cb success
+assertFalse reason true = ExceptT $ ContT \cb -> cb $ failure reason
 
 testC :: forall e. (ContT Unit (Eff e) TestResult) -> Assertion e
-testC c = ErrorT $ ContT \cb -> runContT c cb
+testC c = ExceptT $ ContT \cb -> runContT c cb
 
 testFn :: forall e. ((TestResult -> Eff e Unit) -> Eff e Unit) -> Assertion e
 testFn f = testC $ ContT f
 
 assertC :: forall e. String -> (ContT Unit (Eff e) Boolean) -> Assertion e
-assertC reason c = ErrorT $ ContT \cb -> runContT c \res -> if res then cb (Right unit) else cb (Left reason)
+assertC reason c = ExceptT $ ContT \cb -> runContT c \res -> if res then cb (Right unit) else cb (Left reason)
 
 assertFn :: forall e. String -> ((Boolean -> Eff e Unit) -> Eff e Unit) -> Assertion e
 assertFn reason f = assertC reason $ ContT f
@@ -83,7 +83,7 @@ runWithStderr l t cb = do
   savePos
   print "\x2192 Running: "
   printLabel l
-  runContT (runErrorT t) handler
+  runContT (runExceptT t) handler
   where handler (Right _) = do
           restorePos
           eraseLine
@@ -102,7 +102,7 @@ runWithStderr l t cb = do
           cb error
 
 runWithConsole l t cb =
-  runContT (runErrorT t) handler
+  runContT (runExceptT t) handler
   where handler (Right _) = do
           consoleLog $ "\x2713 Passed: " ++ l
           cb success
@@ -112,12 +112,12 @@ runWithConsole l t cb =
 
 test :: forall e. String -> Assertion (testOutput :: TestOutput | e) -> Test (testOutput :: TestOutput | e)
 test l t =
-  ErrorT $ ContT if hasStderr then runWithStderr l t else runWithConsole l t
+  ExceptT $ ContT if hasStderr then runWithStderr l t else runWithConsole l t
 
 foreign import exit :: forall e. Int -> Eff (testOutput :: TestOutput | e) Unit
 
 runTest :: forall e. Test (testOutput :: TestOutput | e) -> Eff (testOutput :: TestOutput | e) Unit
 runTest t = do
-  runContT (runErrorT t) handler
+  runContT (runExceptT t) handler
   where handler (Left reason) = exit 1
         handler _ = exit 0
